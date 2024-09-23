30e5513089495c16330a5a68b9c0242e686947bc
ShravaniDRaut <shravaniraut1708@gmail.com>
2024-09-23 11:09:13 +0530

    Add files via upload

---
+++ program5.dart
@@ -0,0 +1,27 @@
+import 'dart:io';
+void main(){
+print("enter the number of rows");
+int rows=int.parse(stdin.readLineSync()!);
+
+for(int i=1;i<=rows;i++){
+  int n=1;
+  //space
+  for(int s=1;s<=rows-i;s++){
+    stdout.write("  ");
+  }
+  //pattern symbol
+  for(int j=1;j<=(2*i-1);j++){
+    stdout.write("$n ");
+    if(j>=i){
+      n--;
+    }else{
+      n++;
+    }
+  }
+  print("");
+  
+   
+}
+
+
+}
