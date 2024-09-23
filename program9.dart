08923de9cc07c5f8d1fca5dfa6e6c28ab62b6c2a
ShravaniDRaut <shravaniraut1708@gmail.com>
2024-09-23 11:11:14 +0530

    Update program9.dart

---
+++ program9.dart
@@ -0,0 +1,20 @@
+import 'dart:io';
+void main(){
+print("enter the number of rows");
+int rows=int.parse(stdin.readLineSync()!);
+int n=1;
+for(int i=1;i<=rows;i++){
+  //space
+  for(int s=1;s<=i-1;s++){
+    stdout.write("  ");
+  }
+  //pattern symbol
+  for(int j=1;j<=(2*(rows-i)+1);j++){
+    stdout.write("$n ");
+    n++;
+  }
+  print("");
+  
+   
+}
+}
